1fc1bd0680845fefd0a6f4a10bd91725719d3195
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2021-01-10 19:06:44 -0300

    lendo post no banco de dados

--- website/views.py
+++ website/views.py
@@ -1,7 +1,14 @@
-
 from django.shortcuts import render
+from .models import Post
+
 
 def hello_blog(request):
-    lista = ["django", "python", "html5","git"]
-    data = {"name": "curso de django3", "lista_tecnologia":lista}
+    lista = ["django", "python", "html5", "git"]
+    list_posts = Post.objects.all()
+    data = {"name": "curso de django3",
+            "lista_tecnologia": lista,
+            "posts":list_posts
+            }
+
+
     return render(request, "index.html", data)
