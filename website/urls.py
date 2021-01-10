dda6eb1ccb2e90d3492d54c2123a3014085cf55d
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2021-01-10 14:33:29 -0300

    aulas sobre os templates

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import hello_blog
+
+
+urlpatterns = [
+    path('', hello_blog),
+
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = ["django", "python", "html5","git"]
+    data = {"name": "curso de django3", "lista_tecnologia":lista}
+    return render(request, "index.html", data)
