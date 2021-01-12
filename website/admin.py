821af6cb97acea4c75c411006167a08df5c3e79a
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2021-01-11 21:25:51 -0300

    construindo o formulário do contacto

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from website.models import Post
+from website.models import Post, Contact
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,6 +8,5 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ["title", "sub_title"]
 
 
-
-
 admin.site.register(Post, PostAdmin)
+admin.site.register(Contact)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Contact
 
 
 def hello_blog(request):
@@ -7,11 +7,22 @@ def hello_blog(request):
     list_posts = Post.objects.all()
     data = {"name": "curso de django3",
             "lista_tecnologia": lista,
-            "posts":list_posts
+            "posts": list_posts
             }
 
     return render(request, "index.html", data)
 
+
 def post_detail(request, id):
     post = Post.objects.get(id=id)
-    return render(request, "post_detail.html", {"post":post})
+    return render(request, "post_detail.html", {"post": post})
+
+
+def save_form(request):
+    name = request.POST["name"]
+    Contact.objects.create(
+        name=name,
+        email=request.POST["email"],
+        message=request.POST["message"]
+    )
+    return render(request, "contact_success.html", {"name_contact": name})
